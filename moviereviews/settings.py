6faa52bcac2f015d241da5555330869e85c4e3e8
jdmartinev <110254820+jdmartinev@users.noreply.github.com>
2025-10-27 10:42:23 -0500

    Update settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#k@hv@0z7g=f3cj$p=v&4(_5h8q81h^jrwi9@8isj=p0hg&a8&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.221.17.11"]
 
 
 # Application definition
